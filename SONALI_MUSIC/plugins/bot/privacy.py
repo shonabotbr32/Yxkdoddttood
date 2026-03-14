b69275a070e52edd3fe1e8247695de746015070e
shonabotbr32 <shonabots@gmail.com>
2026-03-15 04:47:32 +0530

    Update privacy.py

--- SONALI_MUSIC/plugins/bot/privacy.py
+++ SONALI_MUSIC/plugins/bot/privacy.py
@@ -1,11 +1,3 @@
-# =======================================================
-# ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
-
-# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
- 
-# 📩 DM for permission : @TheSigmaCoder
-# =======================================================
-
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from SONALI_MUSIC import app
@@ -17,7 +9,7 @@ async def privacy_command(client: Client, message: Message):
         caption="**➻ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ 𝗦𝗵𝗼𝗻𝗮 𝗡𝗲𝘁𝘄𝗼𝗿𝗸 ʙᴏᴛꜱ ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ.**\n\n**⊚ ᴄʟɪᴄᴋ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛʜᴇɴ ꜱᴇᴇ ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ 🔏**",
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("˹ ᴘʀɪᴠᴀᴄʏ ˼", url="https://telegra.ph/Privacy-Policy--Purvi-Bots-by-ALPHA-BABY-08-06")]
+                [InlineKeyboardButton("˹ ᴘʀɪᴠᴀᴄʏ ˼", url="https://telegra.ph/ANONYMOUS---SHONA-BOTS-03-14")]
             ]
         )
     )
