18be91e34665460e5e02328434f68ce55f19e274
shonabotbr32 <shonabots@gmail.com>
2026-03-16 01:16:33 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -77,7 +77,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
          [
              InlineKeyboardButton(text="< - 𝟤𝟢ˢ", callback_data="seek_backward_20"),
-             InlineKeyboardButton(text="• ᴘʀᴏᴍᴏ •", url=f""),
+             InlineKeyboardButton(text="• ᴘʀᴏᴍᴏ •", url=f"https://t.me/LOVE_SOCIES"),
              InlineKeyboardButton(text="𝟤𝟢ˢ + >", callback_data="seek_forward_20")
          ],
         [
